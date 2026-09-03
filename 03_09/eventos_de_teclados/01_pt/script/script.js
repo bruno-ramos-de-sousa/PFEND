85995c10fc527ad6a3475ee6192601ef5a6434b7
Bruno Ramos <brunoramosdsousa0@gmail.com>
2026-09-03 11:56:43 -0300

    docs: aula 03_09

---
+++ 03_09/eventos_de_teclados/01_pt/script/script.js
@@ -0,0 +1,27 @@
+function acao01() {
+  alert("Evento onkeydown");
+}
+
+function acao02(event) {
+  alert("Evento onkeupress voce teclou: " + event.key);
+}
+
+function acao03() {
+  alert("Evento onkeyup");
+}
+
+function acao04() {
+    alert("Perdeu o foco")
+}
+
+function acao05() {
+    alert("Event onchange")
+}
+
+function teste01() {
+  alert("Scroll capturado com sucesso");
+}
+
+function teste02() {
+  alert("Tamanho da janela alterado " + window.innerWidth + "x" + window.innerHeight);
+}
